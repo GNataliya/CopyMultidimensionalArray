16c93950e23a4dc8ce2d63c58e3a0d82f369ddd9
GNataliya <Nataliya.Graff@gmail.com>
2021-04-30 19:42:58 +0300

    level 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let students = [{name: 'Иванна', age: 24}, {name: 'Александр', age: 27},
+                {name: 'Владимир', age: 25}, {name: 'Денис', age: 26}];
+let group = students;
+group[1].age = 22;
+
+console.log(students);
+console.log(group);
+
