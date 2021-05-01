2333b75905312beea2163340255ce581cecd6cc4
GNataliya <Nataliya.Graff@gmail.com>
2021-05-01 08:05:41 +0300

    final result

--- js/main.js
+++ js/main.js
@@ -7,17 +7,19 @@ console.log(students);
 console.log(group);
 
 
-
-let group2 = students.map(array => {return {...array}});
+let students2 = [{name: 'Иванна', age: 24}, {name: 'Александр', age: 27},
+                {name: 'Владимир', age: 25}, {name: 'Денис', age: 26}];
+let group2 = students2.map(array => {return {...array}});
 group2[1].age = 22;
 
-console.log(students);
-console.log(group);
+console.log(students2);
+console.log(group2);
 
 
-
+let students3 = [{name: 'Иванна', age: 24}, {name: 'Александр', age: 27},
+                {name: 'Владимир', age: 25}, {name: 'Денис', age: 26}];
 let group3 = jQuery.extend(true, { }, students);
 group3[1].age = 22;
 
-console.log(students);
-console.log(group);
+console.log(students3);
+console.log(group3);
